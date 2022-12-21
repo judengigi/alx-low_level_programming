55015726261dc646d7ee12b489933fdabebee3c2
judengigi <judengigi9@gmail.com>
2022-12-21 11:23:18 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -9,3 +13,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+
+#endif
